65018897c1a5a18da47b2dbb86a667c141e5daeb
Rody Davis <rody.davis.jr@gmail.com>
2020-02-11 22:47:14 -0800

    updating for dart native

--- bin/ngflutter.dart
+++ bin/ngflutter.dart
@@ -10,15 +10,28 @@ import 'dart:io';
 import 'package:ngflutter/src/command_runner.dart';
 import 'package:args/command_runner.dart';
 
-Future main(List<String> args) async {
-  var runner = new NgDartCommanderRunner();
+// Future main(List<String> args) async {
+//   var runner = new NgDartCommanderRunner();
 
-  try {
-    await runner.run(args);
-  } on UsageException catch (error) {
+//   try {
+//     await runner.run(args);
+//   } on UsageException catch (error) {
+//     print(error);
+//     print(runner.usage);
+//     // Exit code 64 indicates a usage error.
+//     Future.wait([stdout.close(), stderr.close()]).then((_) => exit(64));
+//   }
+// }
+
+void main(List<String> arguments) {
+  // 0 = Success
+  // 1 = Warnings
+  // 2 = Errors
+  exitCode = 0;
+  var runner = NgDartCommanderRunner();
+  runner.run(arguments).catchError((error) {
+    if (error is! UsageException) throw error;
     print(error);
-    print(runner.usage);
-    // Exit code 64 indicates a usage error.
-    Future.wait([stdout.close(), stderr.close()]).then((_) => exit(64));
-  }
+    exit(64); // Exit code 64 indicates a usage error.
+  });
 }
